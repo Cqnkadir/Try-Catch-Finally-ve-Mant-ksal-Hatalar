cfa48893bca387bd8c6f87b88a9f52d62288b1a1
KadirCAN <vakabond29@gmail.com>
2022-03-25 14:04:23 +0300

    s

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace hatayönetimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+            Console.WriteLine("Bir Sayı Giriniz:");
+            int sayı = Convert.ToInt32(Console.ReadLine));
+            Console.WriteLine("Girmiş oldugunuz sayı :" + sayı);
+            }
+            catch(Exception ex) 
+            {
+                Console.WriteLine("Hata: " + ex.Message.ToString());
+            }
+            finally
+            {
+                Console.WriteLine("İşlem tamamlandı");
+            }
+
+
+
+        }
+    }
+}
