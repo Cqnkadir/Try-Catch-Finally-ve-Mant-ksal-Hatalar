7914c0c22d5d9e43e33d934932ba32b1679ea60d
KadirCAN <vakabond29@gmail.com>
2022-03-25 14:50:06 +0300

    son bitti.

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace hatayönetimi
             try
             {
             Console.WriteLine("Bir Sayı Giriniz:");
-            int sayı = Convert.ToInt32(Console.ReadLine));
+            int sayı = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Girmiş oldugunuz sayı :" + sayı);
             }
             catch(Exception ex) 
@@ -21,7 +21,29 @@ namespace hatayönetimi
                 Console.WriteLine("İşlem tamamlandı");
             }
 
-
+            try
+            {
+              // int a = int.Parse(null);  
+              //int a = int.Parse("test");
+              int a = int.Parse("-200000000000");
+            }
+            catch (ArgumentNullException ex)
+            {
+                
+              Console.WriteLine("Boş Deger Girme lan : ");
+              Console.WriteLine(ex);  
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine("Veri toplama uygun değil");
+                Console.WriteLine(ex);
+            }
+            catch(OverflowException ex)
+            {
+                Console.WriteLine("Çok Küçük yada çok büyük bir değer girdiniz");
+                Console.WriteLine(ex);
+            }    
+           
 
         }
     }
